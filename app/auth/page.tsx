4b8f3936c44e14f4441d5570a16e35a087a7d029
boro-dach <monstriqq1@gmail.com>
2025-01-19 01:42:10 +0100

    base authorization logic

---
+++ app/auth/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import LoginForm from '@/components/auth-form'
+import React from 'react'
+
+const Auth = () => {
+
+
+
+  return (
+    <div>
+        <LoginForm/>
+    </div>
+  )
+}
+
+export default Auth
