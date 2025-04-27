edd9334c40d0c33214239e1231e8ebef00d10777
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-04-27 20:20:52 +0200

    save access token when register

--- features/auth-form/register/api/register.ts
+++ features/auth-form/register/api/register.ts
@@ -1,3 +1,4 @@
+import { saveAccessToken } from "@/entities/user/lib/token";
 import axios from "axios";
 
 export async function register(data: any) {
@@ -5,6 +6,14 @@ export async function register(data: any) {
     "http://localhost:5000/auth/register",
     data
   );
+
+  if (response.status !== 200) {
+    throw new Error("Failed to register user");
+  }
+
+  saveAccessToken(response.data.accessToken, {
+    expiresInSeconds: 60 * 60 * 24,
+  });
 
   return response.data;
 }
